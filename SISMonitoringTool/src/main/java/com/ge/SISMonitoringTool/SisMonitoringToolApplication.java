95a1c7289b5aa13517987dccac1848bdc47ef574
magantilavakumar <=>
2021-05-09 23:22:30 +0800

    Base Version

---
+++ SISMonitoringTool/src/main/java/com/ge/SISMonitoringTool/SisMonitoringToolApplication.java
@@ -0,0 +1,13 @@
+package com.ge.SISMonitoringTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisMonitoringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisMonitoringToolApplication.class, args);
+	}
+
+}
---
+++ SISMonitoringTool/src/test/java/com/ge/SISMonitoringTool/SisMonitoringToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ge.SISMonitoringTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisMonitoringToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
